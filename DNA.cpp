d4a575a21ffd54090440357fba8156fc06057d9e
Carolina Nóbrega <notcarolinna@Gmail.com>
2023-08-20 21:14:05 -0300

    Update DNA.cpp

--- DNA.cpp
+++ DNA.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <string>
-
-/*
-letraConsecutiva -> é a letra que possui um número x de repetições consecutivas na cadeia e é atualizada
-sempre que a sequência for interrompida.
-letraAnalisada -> guarda a letra que está sendo analisada no momento, é utilizada para comparar com a 'letraConsecutiva'
-com a finalidade de determinar se há repetições consecutivas ou se a sequência foi interrompida. 
-*/
-
+#include <cmath>
 
 std::string getLetraComplementar(char letraConsecutiva, char letraAnalisada) {
     if (letraConsecutiva == 'A') {
@@ -26,34 +20,51 @@ std::string getLetraComplementar(char letraConsecutiva, char letraAnalisada) {
 }
 
 int main() {
-    std::string cadeiaDNA = "DNANDANDANDANADNDDDAN";
-    char letraConsecutiva = cadeiaDNA[0];
-    int repeticoes = 1;
-
-    for (size_t i = 1; i < cadeiaDNA.length(); i++) {
-        std::string log = "";
-        for (int j = 0; j < repeticoes; j++) {
-            log += letraConsecutiva;
-        }
-        std::cout << log << " " << cadeiaDNA.substr(i) << std::endl;
-
-        char letraAnalisada = cadeiaDNA[i];
-
-        if (letraAnalisada == letraConsecutiva) {
-            repeticoes += 1;
-        }
-        else {
-            std::string letraComplementar = getLetraComplementar(letraConsecutiva, letraAnalisada);
-            letraConsecutiva = (pow(-1, repeticoes) == 1) ? letraAnalisada : letraComplementar[0];
-            repeticoes = 1;
-        }
+    std::ifstream inputFile("teste.txt"); 
+    if (!inputFile) {
+        std::cerr << "Erro ao abrir o arquivo." << std::endl;
+        return 1;
     }
 
-    std::string result = "";
-    for (int i = 0; i < repeticoes; i++) {
-        result += letraConsecutiva;
+    std::string cadeiaDNA;
+
+    if (std::getline(inputFile, cadeiaDNA)) {
+        inputFile.close();
+        char letraConsecutiva = cadeiaDNA[0];
+        int repeticoes = 1;
+
+        for (size_t i = 1; i < cadeiaDNA.length(); i++) {
+            std::string log = "";
+            for (int j = 0; j < repeticoes; j++) {
+                log += letraConsecutiva;
+            }
+
+            // A linha abaixo mostra todo o processo de simplificação da cadeia de DNA
+            // std::cout << log << " " << cadeiaDNA.substr(i) << std::endl;
+
+            char letraAnalisada = cadeiaDNA[i];
+
+            if (letraAnalisada == letraConsecutiva) {
+                repeticoes += 1;
+            }
+            else {
+                std::string letraComplementar = getLetraComplementar(letraConsecutiva, letraAnalisada);
+                letraConsecutiva = (pow(-1, repeticoes) == 1) ? letraAnalisada : letraComplementar[0];
+                repeticoes = 1;
+            }
+        }
+
+        std::string result = "";
+        for (int i = 0; i < repeticoes; i++) {
+            result += letraConsecutiva;
+        }
+        std::cout << result << std::endl;
     }
-    std::cout << result << std::endl;
+    else {
+        std::cerr << "Erro ao ler o arquivo." << std::endl;
+        inputFile.close();
+        return 1;
+    }
 
     return 0;
 }
